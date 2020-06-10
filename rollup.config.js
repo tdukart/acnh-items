70ebca38cc5dc1224e603e95e0a779d90dca61c8
Todd Dukart <tdukart@mechanicalninja.com>
2020-06-10 19:54:16 -0400

    Generate ES6 module

--- rollup.config.js
+++ rollup.config.js
@@ -3,15 +3,24 @@ import external from 'rollup-plugin-peer-deps-external';
 import json from 'rollup-plugin-json';
 import resolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
+import pkg from './package.json'
 
 module.exports = {
   input: './src/index.ts',
-  output: {
-    dir: './dist',
-    format: 'umd',
-    name: 'library',
-    sourcemap: true,
-  },
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      exports: 'named',
+      sourcemap: true,
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      exports: 'named',
+      sourcemap: true,
+    },
+  ],
   plugins: [
     commonjs(),
     external(),
